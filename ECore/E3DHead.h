06d2683b22e851474162258d28b66af0b1dc0e2c
sparxcrossroads <sparxcrossroads@sina.com>
2018-11-23 00:58:31 +0800

    重现建立c++ 静态编译库 ECore.lib 添加头文件 E3DHead.h ECommon.h

---
+++ ECore/E3DHead.h
@@ -0,0 +1,21 @@
+#pragma once
+
+
+namespace E3D
+{
+	struct EColor;					// 颜色
+	struct EVector2D;				// 二位向量
+	struct EVector3D;				// 三维向量
+	struct EVctror4D;				// 四维向量
+	struct EMatrix44;				// 4x4矩阵
+
+	struct EVertex4D;				// 顶点
+	struct EPolygon4D;				// 基于顶点多边形
+	struct EPolygonF4D;				// 基于索引多边形
+	struct EObject4D;				// 对象
+	struct EPlane3D;				// 三维平面
+	struct ERenderList4D;			// 渲染队列
+
+	struct EFrustum;				// 透视体
+
+}
